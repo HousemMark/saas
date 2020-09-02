4a7f1260e3d577a242adaf21bb648a64cfd73b14
mazhonghao <893923042@qq.com>
2020-09-02 11:06:27 +0800

    增加log4j

--- merchant/merchant-web/src/main/java/com/unit/merchant/web/controller/MerchantBaseController.java
+++ merchant/merchant-web/src/main/java/com/unit/merchant/web/controller/MerchantBaseController.java
@@ -2,6 +2,7 @@ package com.unit.merchant.web.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: 小蜘蛛(mazhonghao)
  * @create: 2020-09-1 17:37
  **/
+@Slf4j
 @Api("基本操作页面")
 @RequestMapping("/base")
 @RestController
