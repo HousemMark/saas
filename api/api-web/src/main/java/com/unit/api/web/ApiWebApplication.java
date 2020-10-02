9cad7d59105de112d19fccc838eb92a4cb951c05
mazhonghao <893923042@qq.com>
2020-10-02 13:03:03 +0800

    basic-zk built

--- api/api-web/src/main/java/com/unit/api/web/ApiWebApplication.java
+++ api/api-web/src/main/java/com/unit/api/web/ApiWebApplication.java
@@ -4,27 +4,36 @@ package com.unit.api.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.cloud.netflix.zuul.filters.route.RibbonCommandFactory;
 import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Collections;
 import java.util.Set;
 
-@SpringBootApplication(scanBasePackages = {"com.unit.*"})
+@SpringBootApplication (scanBasePackages = {"com.unit.*"})
 @EnableZuulProxy
-//@EnableFeignClients(basePackages = "com.unit.api.core.*")
+@EnableFeignClients (basePackages = "com.unit.api.common.*")
 public class ApiWebApplication {
 
-    @Autowired(required = false)
+    @Autowired (required = false)
     private Set<ZuulFallbackProvider> zuulFallbackProviderSet = Collections.emptySet();
 
     public static void main(String[] args) {
         SpringApplication.run(ApiWebApplication.class, args);
     }
 
+//    /**
+//     * 负载均衡
+//     */
 //    @Bean
 //    @ConditionalOnMissingBean
 //    public RibbonCommandFactory<?> ribbonCommandFactory(SpringClientFactory clientFactory, ZuulProperties zuulProperties) {
-//        return new UnitHttpCliemtRibbonCommandFactory(clientFactory,zuulProperties, zuulFallbackProviderSet);
+//        return new UnitHttpCliemtRibbonCommandFactory(clientFactory, zuulProperties, zuulFallbackProviderSet);
 //    }
 }
